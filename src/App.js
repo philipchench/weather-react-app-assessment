411ca17bf204738fe7e09cb0368b7f6f4bdfa42f
Philip Chen <philipandrewchen@gmail.com>
2021-12-02 23:05:11 -0600

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+const apiKey = "d918fb09ac8966283f0131e75067b145"
+const apiLink = "https://api.openweathermap.org/data/2.5/"
 
 function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try{
+      const res = await fetch(`${apiLink}weather?q=${city}&units=metric&APPID=${apiKey}`)
+      const resData = await res.json()
+      console.log(resData)
+    }catch(err){
+
+    }
+
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className = "search" onSubmit={onSubmit}>
+            <label>Search city</label>
+            <input
+              type='text'
+              placeholder='Enter city...'
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <input type='submit' value='Search' />
+        </form>
+
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            {/* <div className="date">{dateBuilder(new Date())}</div> */}
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+        ) : (
+          ''
+        )}
+
     </div>
   );
 }
